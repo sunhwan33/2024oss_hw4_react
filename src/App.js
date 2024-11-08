0792d7d4f03496e0a6e5d429157df28cabc93e3b
sunhwan33 <sunhwan33@gmail.com>
2024-11-08 17:42:09 +0900

    edit App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Hello from './components/Hello';
+import Course from './components/Course';
+import Myinfo from './components/Myinfo';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hello/>
+        <Course/>
+        <Myinfo/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.ddd
+          Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
